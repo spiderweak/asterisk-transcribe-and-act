d556dad461e3bfa4b6841d2501bbee836e2b242a
Spiderweak <spiderweak@a-bernard.fr>
2024-03-16 14:33:52 -0400

    index

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
 from .custom_exceptions import MissingPackageError
-from .audio_utils import check_ffmpeg_installed
+from .audio_utils import check_ffmpeg_installed, get_wav_duration, cut_beginning
 from .config import Config
-from .file_utils import generate_folder
+from .file_utils import generate_folder, get_pair_path
