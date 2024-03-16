b1ce9284938781174480e9601440118a7d51f2c8
Spiderweak <spiderweak@a-bernard.fr>
2024-03-16 14:33:42 -0400

    feat: adds processing queue and removes processing dupplication between modified and created

--- app/transcriber/watcher.py
+++ app/transcriber/watcher.py
@@ -1,156 +1,84 @@
 import time
 import os
-import pprint
 import logging
 
+from typing import Optional
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 
 from .transcriber import AudioTranscriptionManager
-from ..utils import  generate_folder
+from ..utils import  generate_folder, get_pair_path
 
 
 class Watcher:
-    def __init__(self, watch_directory):
+    def __init__(self, watch_directory, processing_queue):
         self.watch_directory = watch_directory
+        self.processing_queue = processing_queue
         self.observer = Observer()
 
     def run(self):
-        event_handler = Handler()
+        event_handler = EventHandler(self.processing_queue)
         self.observer.schedule(event_handler, self.watch_directory, recursive=True)
         self.observer.start()
         try:
             while True:
-                time.sleep(5)
+                time.sleep(1)
         except KeyboardInterrupt:
             self.observer.stop()
         self.observer.join()
 
-class Handler(FileSystemEventHandler):
-    def __init__(self):
-        self.processed_created_pairs = set()
-        self.processed_modified_pairs = set()
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, processing_queue):
         self.atm_data = dict()
+        self.processing_queue = processing_queue
 
     def on_created(self, event):
-        if event.is_directory:
-            return None
+        if event.is_directory or not event.src_path.endswith('.wav'):
+            return
 
         logging.debug(f"Received created event - {event.src_path}.")
         file_path = event.src_path
-        pair_path = self.get_pair_path(file_path)
+        pair_path = get_pair_path(file_path)
 
-        if pair_path is None or not os.path.exists(pair_path):
-            logging.debug(f"Waiting for associated file for {file_path}.")
-        else:
-            self.process_created_files(file_path, pair_path)
-            logging.debug(f"Associated file found, processing pair")
-
-    def get_pair_path(self, file_path: str):
-        """
-        Given one file of a pair, returns the expected path of its pair.
-        Assumes file naming convention of '...-in.wav' and '...-out.wav'.
-        """
-        if "-in.wav" in file_path:
-            return file_path.replace("-in.wav", "-out.wav")
-        elif "-out.wav" in file_path:
-            return file_path.replace("-out.wav", "-in.wav")
-        return None
-
-    def process_created_files(self, file_path, pair_path):
-        """
-        Process the pair of files for transcription.
-        """
-        if (file_path, pair_path) in self.processed_created_pairs or (pair_path, file_path) in self.processed_created_pairs:
-            # Avoid processing the same pair twice.
-            return
-
-        # Ensure to mark this pair as processed to avoid re-processing.
-        self.processed_created_pairs.add((file_path, pair_path))
-
-        if "in" in file_path:
-            in_path = file_path
-            out_path = pair_path
-        else:
-            in_path = pair_path
-            out_path = file_path
-
-        unique_identifier = '-'.join(os.path.basename(file_path).split('-')[:-1])
-
-        folder_name = generate_folder(unique_identifier)
-        logging.debug(f"Generating Temporary folder : {folder_name}")
-
-        # Merge files, if necessary.
-        logging.debug(f"Merging Audio ?")
-        # Run transcription.
-        logging.debug(f"Creating associated transcriber object")
-        transcription_manager = AudioTranscriptionManager(in_path, out_path, folder_name)
-
-        self.atm_data[unique_identifier] = transcription_manager
-
-        logging.debug(f"Transcribing conversation between {file_path} and {pair_path}.")
-
-        logging.debug(f"Starting conversation renderer watchdog")
-
+        if pair_path and os.path.exists(pair_path):
+            self.process_files(file_path, pair_path)
 
     def on_modified(self, event):
-        if event.is_directory:
-            return None
+        if event.is_directory or not event.src_path.endswith('.wav'):
+            return
 
         logging.debug(f"Received modified event - {event.src_path}.")
         file_path = event.src_path
-        pair_path = self.get_pair_path(file_path)
+        pair_path = get_pair_path(file_path)
 
-        if pair_path is None or not os.path.exists(pair_path):
-            logging.debug(f"Waiting for associated file for {file_path}.")
-        else:
-            self.process_modified_files(file_path, pair_path)
-            logging.debug(f"Associated file found, processing pair")
+        if pair_path and os.path.exists(pair_path):
+            transcription_manager  = self.process_files(file_path, pair_path)
 
-
-    def process_modified_files(self, file_path, pair_path):
-        """
-        Process the pair of files for transcription.
-        """
-        if (file_path, pair_path) in self.processed_modified_pairs or (pair_path, file_path) in self.processed_modified_pairs:
-            # Avoid processing the same pair twice.
-            return
+            if transcription_manager is None or transcription_manager in self.processing_queue:
+                # Pair was already processed recently
+                logging.debug(f"No action for this event, action already done or currently processing")
+                return
+            else:
+                logging.debug(f"Appending transcription request to queue")
+                self.processing_queue.append(transcription_manager)
 
 
-        if "in" in file_path:
-            in_path = file_path
-            out_path = pair_path
-        else:
-            in_path = pair_path
-            out_path = file_path
+    def process_files(self, file_path, pair_path) -> Optional[AudioTranscriptionManager]:
+        # Determine in_path and out_path based on file naming
+        in_path, out_path = (file_path, pair_path) if "in" in file_path else (pair_path, file_path)
 
-        # Locks both files
-        self.processed_modified_pairs.add((file_path, pair_path))
-
+        # Extract unique identifier from the file name (assuming a certain naming convention)
         unique_identifier = '-'.join(os.path.basename(file_path).split('-')[:-1])
 
+        # Generate or retrieve the temporary folder for this conversation
         folder_name = generate_folder(unique_identifier)
-        logging.debug(f"Getting Temporary folder name : {folder_name}")
+        logging.debug(f"Temporary folder for conversation: {folder_name}")
 
-        # Merge files, if necessary.
-        logging.debug(f"Merging Audio ?")
+        # Initialize or retrieve the AudioTranscriptionManager for this conversation
+        if unique_identifier not in self.atm_data:
+            self.atm_data[unique_identifier] = AudioTranscriptionManager(in_path, out_path, folder_name)
 
-        try:
-            transcription_manager = self.atm_data[unique_identifier]
-        except KeyError:
-            transcription_manager = self.atm_data[unique_identifier] = AudioTranscriptionManager(in_path, out_path, folder_name)
+        transcription_manager = self.atm_data[unique_identifier]
 
-        logging.debug(f"Running transcription handler")
-        inbound, outbound = transcription_manager.transcribe()
-    
-        pprint.pprint(inbound)
-        pprint.pprint(outbound)
-
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
-
-        else:
-            logging.debug(f"No action associated with event {event.event_type}")
-            return None
+        return transcription_manager
