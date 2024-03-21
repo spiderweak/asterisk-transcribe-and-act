6a48b38cb5b5e9213530b14d54da9ffbffadad7d
Spiderweak <spiderweak@a-bernard.fr>
2024-03-21 09:29:10 -0400

    adds write to file

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
 from .custom_exceptions import MissingPackageError
 from .audio_utils import check_ffmpeg_installed, get_wav_duration, cut_beginning
 from .config import Config
-from .file_utils import generate_folder, get_pair_path
+from .file_utils import generate_folder, get_pair_path, write_to_file
